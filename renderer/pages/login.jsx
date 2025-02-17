5a45dcf92282faf9ea2145f62291d7e28d044a3c
SlothyKoala <aneethling21@gmail.com>
2025-02-17 23:44:55 +0200

    modified:   renderer/pages/login.jsx 	modified:   renderer/pages/register.jsx 	new file:   renderer/styles/login-register-page.module.css

--- renderer/pages/login.jsx
+++ renderer/pages/login.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import LoginForm from '../components/LoginForm';
 import Header from '../components/Header';
+import styles from '../styles/login-register-page.module.css';
 
 const LoginPage = () => {
   return (
     <>
       <Header />
-      <div>
-        <h1>Login</h1>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Login</h1>
         <LoginForm />
       </div>
     </>
--- renderer/pages/register.jsx
+++ renderer/pages/register.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import RegisterForm from '../components/RegisterForm';
 import Header from '../components/Header';
+import styles from '../styles/login-register-page.module.css';
 
 const RegisterPage = () => {
   return (
     <>
       <Header />
-      <div>
-        <h1>Register</h1>
+      <div className={styles.container}>
+        <h1 className={styles.title}>Register</h1>
         <RegisterForm />
       </div>
     </>
